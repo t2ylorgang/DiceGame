1fb790eb6febbd9795c6145eacd9cbadf06c217a
Taylor <hillking2017@gmail.com>
2023-01-03 22:00:33 -0800

    added function to change src link to different dice image for dice one.

--- index.js
+++ index.js
@@ -7,6 +7,27 @@ function getRandomInt(max) {
 var randomNumber1 = getRandomInt(6);
 
 // These 3 lines of code select the images from the DOM and store them as seperate variables from the array that is returned using querySelectorAll.
-var diceImages = document.querySelectorAll("img")
+const diceImages = document.getElementsByTagName("img")
 var diceOneImg = diceImages[0];
 var diceTwoImg = diceImages[1];
+
+// This function changes the src link for the corresponding image based on what randomNumber1 is.
+function getRandomDice1() {
+    if (randomNumber1 === 1) {
+        diceOneImg.src = "images/dice1.png";
+    } else if (randomNumber1 === 2) {
+        diceOneImg.src = "images/dice2.png"
+    } else if (randomNumber1 === 3) {
+        diceOneImg.src = "images/dice3.png"
+    } else if (randomNumber1 === 4) {
+        diceOneImg.src = "images/dice4.png"
+    } else if (randomNumber1 === 5) {
+        diceOneImg.src = "images/dice5.png"
+    } else {
+        diceOneImg.src = "images/dice6.png"
+    }
+}
+
+getRandomDice1();
+
+console.log(randomNumber1);
