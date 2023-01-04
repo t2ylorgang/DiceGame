8979d3faa103d16b21bbb9e7914cf1cbd252d2e2
Taylor <hillking2017@gmail.com>
2023-01-03 20:55:57 -0800

    Used querySelectorAll to return an array of the 2 images I am going to manipulate with JavaScript. Also seperated images out from array by setting them equal to indices of said array.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-// This function returns a random number between 1 and 6.
-
+// This function returns a random number with a "max" parameter.
 function getRandomInt(max) {
     return (Math.floor(Math.random()*max))+1;
 }
 
+// Here I am setting a variable to the output of the random integer function (1-6) for further use.
 var randomNumber1 = getRandomInt(6);
+
+// These 3 lines of code select the images from the DOM and store them as seperate variables from the array that is returned using querySelectorAll.
+var diceImages = document.querySelectorAll("img")
+var diceOneImg = diceImages[0];
+var diceTwoImg = diceImages[1];
